25cbccfe592c9f7c8267342ba468a8f8f2ffbaa9
Quentin BELLINO <quentin.bellino@C02GQ7QJQ6LR-MPL2FR.local>
2023-06-16 15:26:06 +0200

    metamask connect button + hashlips

---
+++ dapp-metamask/src/App.tsx
@@ -0,0 +1,45 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+import detectEthereumProvider from '@metamask/detect-provider'
+
+const App = () => {
+  const [hasProvider, setHasProvider] = useState<boolean | null>(null)
+  const initialState = { accounts: [] }               /* New */
+  const [wallet, setWallet] = useState(initialState)  /* New */
+
+  useEffect(() => {
+    const getProvider = async () => {
+      const provider = await detectEthereumProvider({ silent: true })
+      setHasProvider(Boolean(provider))
+    }
+
+    getProvider()
+  }, [])
+
+  const updateWallet = async (accounts:any) => {     /* New */
+    setWallet({ accounts })                          /* New */
+  }                                                  /* New */
+
+  const handleConnect = async () => {                /* New */
+    let accounts = await window.ethereum.request({   /* New */
+      method: "eth_requestAccounts",                 /* New */
+    })                                               /* New */
+    updateWallet(accounts)                           /* New */
+  }                                                  /* New */
+
+  return (
+    <div className="App">
+      <div>Metamask {hasProvider ? 'detected.' : 'not detected.'}</div>
+
+      { hasProvider &&                               /* Updated */
+        <button onClick={handleConnect}>Connect MetaMask</button>
+      }
+      
+      { wallet.accounts.length > 0 &&                /* New */
+        <div>Wallet Accounts: { wallet.accounts[0] }</div>
+      }
+    </div>
+  )
+}
+
+export default App
